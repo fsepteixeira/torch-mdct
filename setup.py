b54947e5d6164da9d8505aad8739b70bfbcc216e
fsepteixeira <57730275+fsepteixeira@users.noreply.github.com>
2021-09-14 18:02:40 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ NAME = 'torch_mcdt'
 REQUIREMENTS = [
     'numpy',
     'torch',
+    'torchaudio',
     'scipy',
 ]
 
@@ -16,29 +17,13 @@ EXTRAS={
 
 setup(
     name="torch_mdct",
-    version="0.0.0",
+    version="0.0.1",
     description="Pytorch implementation of MDCT/iMDCT",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/fsept11/torch-mdct",
+    url="https://github.com/fsepteixeira/torch-mdct",
     author="Francisco Teixeira",
     author_email="francisco.s.teixeira@tecnico.ulisboa.pt",
-    classifiers=[
-        "Environment :: Plugins",
-        "Intended Audience :: Developers",
-        "Intended Audience :: Science/Research",
-        "License :: OSI Approved :: BSD License",
-        "Operating System :: MacOS :: MacOS X",
-        "Operating System :: Microsoft :: Windows",
-        "Operating System :: POSIX",
-        "Programming Language :: C++",
-        "Programming Language :: Python :: 3",
-        "Topic :: Multimedia :: Sound/Audio",
-        "Topic :: Scientific/Engineering :: Artificial Intelligence",
-        "Topic :: Scientific/Engineering :: Signal Processing"
-    ],
-    # Exclude the build files.
     packages=find_packages(),
     install_requires=REQUIREMENTS,
-    extras_requires=EXTRAS,
 )
